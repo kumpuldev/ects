ae48de5e29a47ac73baea107013b69abcf934822
adeyahya <adeyahyaprasetyo@gmail.com>
2020-08-15 22:15:51 +0700

    from

--- lib/LinkedList.ts
+++ lib/LinkedList.ts
@@ -1,72 +1,104 @@
 export class ListNode<T> {
-  value: T;
-  next: ListNode<T> | null;
-  prev: ListNode<T> | null;
-  constructor(value: T) {
-    this.value = value;
-    this.next = null;
-    this.prev = null;
-  }
+	value: T;
+	next: ListNode<T> | null;
+	prev: ListNode<T> | null;
+	constructor(value: T) {
+		this.value = value;
+		this.next = null;
+		this.prev = null;
+	}
 }
 
 export class LinkedList<T> {
-  length: number;
-  head: ListNode<T> | null;
-  tail: ListNode<T> | null;
-  constructor(value?: T, ...rest: Array<T>) {
-    this.length = 0;
-    this.head = null;
-    this.tail = null;
-    if (value) {
-      this.push(value, ...rest);
-    }
-  }
+	length: number;
+	head: ListNode<T> | null;
+	tail: ListNode<T> | null;
+	constructor(value?: T, ...rest: Array<T>) {
+		this.length = 0;
+		this.head = null;
+		this.tail = null;
+		if (value) {
+			this.push(value, ...rest);
+		}
+	}
 
-  /**
+	/**
+   * creates a new, shallow-copied LinkedList instance from an array-like or iterable object.
+   * @param value - Iterable object
+   * @returns LinkedList
+   */
+	static from(value: string | object | Array<unknown>) {
+		const valueType = typeof value;
+
+		if (!value || valueType === "number") {
+			throw new TypeError(`${value} is not iterable`);
+		}
+
+    const list = new LinkedList<unknown>();
+    const valueArray = [].slice.call(value);
+    list.pushArray(valueArray);
+		return list;
+	}
+
+	/**
    * Determine wheter the given value is an LinkedList
    * @param value - any
    * @returns boolean
    */
-  static is(value: unknown) {
-    if (typeof value !== 'object') return false;
-    const val = value as LinkedList<unknown>;
-    // check if it has signature property
-    if (!val.hasOwnProperty('length')) return false;
-    if (!val.hasOwnProperty('head')) return false;
-    if (!val.hasOwnProperty('tail')) return false;
+	static is(value: unknown) {
+		if (typeof value !== "object") {
+			return false;
+		}
+		const val = (value as LinkedList<unknown>);
+		// check if it has signature property
+		if (!val.hasOwnProperty("length")) {
+			return false;
+		}
+		if (!val.hasOwnProperty("head")) {
+			return false;
+		}
+		if (!val.hasOwnProperty("tail")) {
+			return false;
+		}
 
-    // check if it's node is valid
-    if (val.length > 0) {
-      const head = val.head;
-      const tail = val.tail;
+		// check if it's node is valid
+		if (val.length > 0) {
+			const head = val.head;
+			const tail = val.tail;
 
-      // check if it's has signature property of Node
-      if (!head.hasOwnProperty('next')) return false;
-      if (!head.hasOwnProperty('prev')) return false;
-      if (!head.hasOwnProperty('value')) return false;
-    }
+			// check if it's has signature property of Node
+			if (!head.hasOwnProperty("next") || !tail.hasOwnProperty("next")) {
+				return false;
+			}
+			if (!head.hasOwnProperty("prev") || !tail.hasOwnProperty("prev")) {
+				return false;
+			}
+			if (!head.hasOwnProperty("value") || !tail.hasOwnProperty("value")) {
+				return false;
+			}
+		}
 
-    return true;
-  }
+		return true;
+	}
 
 	/**
      * append the rest parameter of method push
      * @param value 
      */
-  private pushArray(value: Array<T>) {
-    value.forEach((n) => {
-      this.push(n);
-    });
-  }
+	private pushArray(value: Array<T>) {
+		value.forEach((n) => {
+			this.push(n);
+		});
+	}
 
 	/**
      * clear all elements
      */
-  clear() {
-    this.length = 0;
-    this.head = null;
-    this.tail = null;
-  }
+	clear() {
+		this.length = 0;
+		this.head = null;
+		this.tail = null;
+	}
 
 	/**
      * @param value 
@@ -74,22 +106,22 @@ export class LinkedList<T> {
      * @example
      *  list.push(1, 2, 4, 6)
      */
-  push(value: T, ...rest: Array<T>) {
-    const node = new ListNode<T>(value);
-    if (this.length === 0) {
-      this.head = node;
-      this.tail = node;
-    }
+	push(value: T, ...rest: Array<T>) {
+		const node = new ListNode<T>(value);
+		if (this.length === 0) {
+			this.head = node;
+			this.tail = node;
+		}
 
-    this.tail.next = node;
-    this.tail = node;
+		this.tail.next = node;
+		this.tail = node;
 
-    this.length++;
+		this.length++;
 
-    // handle multiple arguments
-    // e.g list.push(2, 3)
-    if (rest.length > 0) {
-      this.pushArray(rest);
-    }
-  }
+		// handle multiple arguments
+		// e.g list.push(2, 3)
+		if (rest.length > 0) {
+			this.pushArray(rest);
+		}
+	}
 }
--- test/LinkedList.spec.ts
+++ test/LinkedList.spec.ts
@@ -1,39 +1,59 @@
-import { expect } from "chai";
-import { LinkedList } from "..";
+import {expect} from "chai";
+import {LinkedList} from "../index";
 
-describe("LinkedList", () => {
-  it("should initiate list with one element", () => {
-    const list = new LinkedList<number>(2);
-    expect(list.head.value).eq(2);
-  },
-  );
+describe(
+	"LinkedList",
+	() => {
+		it(
+			"should initiate list with one element",
+			() => {
+				const list = new LinkedList<number>(2);
+				expect(list.head.value).eq(2);
+			},
+		);
 
-  it("should initiate list with multiple element", () => {
-    const list = new LinkedList<number>(2, 3, 4, 5);
-    expect(list.head.value).eq(2);
-    expect(list.head.next.value).eq(3);
-    expect(list.length).eq(4);
-  },
-  );
+		it(
+			"should initiate list with multiple element",
+			() => {
+				const list = new LinkedList<number>(2, 3, 4, 5);
+				expect(list.head.value).eq(2);
+				expect(list.head.next.value).eq(3);
+				expect(list.length).eq(4);
+			},
+		);
 
-  it("should determine whether the given value is an LinkedList", () => {
-    expect(LinkedList.is([2, 3])).eq(false);
-    const list = new LinkedList(2, 3);
-    expect(LinkedList.is(list)).eq(true)
-  })
+		it(
+			"should determine whether the given value is an LinkedList",
+			() => {
+				expect(LinkedList.is([2, 3])).eq(false);
+        const list = new LinkedList(2, 3);
+				expect(LinkedList.is(list)).eq(true);
+			},
+    );
+    
+    it("should conver iterable object into LinkedList", () => {
+      const list = LinkedList.from([2,3,4,5]);
+      expect(LinkedList.is(list)).eq(true);
+      expect(list.length).eq(4);
+      expect(list.head.value).eq(2);
+    })
 
-  it("should push single element", () => {
-    const list = new LinkedList<unknown>();
-    list.push(12);
-    expect(list.head.value).eq(12);
-  },
-  );
+		it(
+			"should push single element",
+			() => {
+				const list = new LinkedList<unknown>();
+				list.push(12);
+				expect(list.head.value).eq(12);
+			},
+		);
 
-  it("should push multiple element", () => {
-    const list = new LinkedList<unknown>();
-    list.push(12, 33, 44);
-    expect(list.length).eq(3);
-  },
-  );
-},
+		it(
+			"should push multiple element",
+			() => {
+				const list = new LinkedList<unknown>();
+				list.push(12, 33, 44);
+				expect(list.length).eq(3);
+			},
+		);
+	},
 );
